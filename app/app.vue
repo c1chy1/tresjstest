0a7b9c8921bc160adde9c6ad88b938d5bf18fe46
cichy <cichy08081987pp@gmail.com>
2026-03-19 23:15:22 +0100

    feat(json) install tresjs

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,36 @@
+<script setup>
+import { shallowRef, onMounted } from 'vue'
+import { OrbitControls } from '@tresjs/cientos'
+
+const model = shallowRef(null)
+
+onMounted(async () => {
+  const { GLTFLoader } = await import('three/examples/jsm/loaders/GLTFLoader.js')
+
+  const loader = new GLTFLoader()
+
+  loader.load('/models/heart.glb', (gltf) => {
+    model.value = gltf.scene
+
+
+  })
+
+
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <client-only>
+    <div style="height:100vh">
+      <TresCanvas window-size>
+        <TresPerspectiveCamera :position="[0,0,2]" />
+
+        <TresAmbientLight />
+        <TresDirectionalLight :position="[1,3,3]" />
+
+        <OrbitControls />
+        <primitive v-if="model" :object="model" />
+      </TresCanvas>
+    </div>
+  </client-only>
 </template>
